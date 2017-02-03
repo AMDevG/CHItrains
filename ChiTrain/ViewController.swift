29af11cd2a5b5c2340304e58d1f6ce735a9a098b
AMDevG <jeberry308@gmail.com>
2017-02-02 23:49:46 -0600

    Initial Commit

---
+++ ChiTrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChiTrain
+//
+//  Created by John Berry on 2/2/17.
+//  Copyright © 2017 dev G Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
