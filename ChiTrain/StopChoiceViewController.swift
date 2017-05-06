9947380caaf2ba1c15dadab005d8f463c616334b
AMDevG <jeberry308@gmail.com>
2017-05-06 16:24:28 -0400

    Sorted Stops

--- ChiTrain/StopChoiceViewController.swift
+++ ChiTrain/StopChoiceViewController.swift
@@ -13,6 +13,8 @@ class StopChoiceViewController: UITableViewController {
     var lineSelection: String!
     var stopNames = [String: String]()
     var selectedStation: String!
+    var sortedNames = [String]()
+    
     @IBOutlet weak var stopLabel: UILabel!
 
     override func viewDidLoad() {
@@ -20,34 +22,42 @@ class StopChoiceViewController: UITableViewController {
         if lineSelection! == "Red"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.redStopIds
+            sortedNames = Array(DestViewController.redStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Blue"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.blueStopIds
+             sortedNames = Array(DestViewController.blueStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Brown"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.brownStopIds
+             sortedNames = Array(DestViewController.brownStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Green"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.greenStopIds
+             sortedNames = Array(DestViewController.greenStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Orange"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.orangeStopIds
+             sortedNames = Array(DestViewController.orangeStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Purple"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.purpleStopIds
+             sortedNames = Array(DestViewController.purpleStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Pink"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.pinkStopIds
+             sortedNames = Array(DestViewController.pinkStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
         if lineSelection! == "Yellow"{
             let DestViewController: StopDictionaryViewController = StopDictionaryViewController()
             stopNames = DestViewController.yellowStopIds
+             sortedNames = Array(DestViewController.yellowStopIds.keys).sorted{ $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
         }
     }
     
@@ -61,7 +71,8 @@ class StopChoiceViewController: UITableViewController {
     
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "stopCell", for: indexPath)
-        let key = Array(self.stopNames.keys)[indexPath.row]
+        //let key = Array(self.stopNames.keys)[indexPath.row]
+        let key = sortedNames[indexPath.row]
         cell.textLabel?.text = key
         return cell
     }
